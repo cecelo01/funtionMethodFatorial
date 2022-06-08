8cf14d617473dbfd6d8c4c66991155b1af41ea23
Marcelo Vieira <48066643+cecelo01@users.noreply.github.com>
2022-06-08 08:40:38 -0300

    up

---
+++ src/funtionmethodfatorial/FuntionMethodFatorial.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funtionmethodfatorial;
+
+/**
+ *
+ * @author Marce
+ */
+public class FuntionMethodFatorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Factorial f = new Factorial();
+        
+        f.setValor(5);
+        System.out.print(f.getFormula());
+        System.out.println(f.getFatorial());
+        
+    }
+    
+}
